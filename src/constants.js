07da8842455a69e6c1e570b63224272b91322f80
Subramanian <selvags199@gmail.com>
2018-11-18 20:29:23 +0530

    Updates

--- src/constants.js
+++
@@ -1,4 +0,0 @@
-export const ChangeSearchField = 'CHANGE_SEARCH_Field';
-export const RequestRobotsPending = 'REQUEST_ROBOTS_PENDING';
-export const RequestRobotsSuccess = 'REQUEST_ROBOTS_SUCCESS';
-export const RequestRobotsFailure = 'REQUEST_ROBOTS_FAILURE';
